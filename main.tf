06e615fcdac09c6fa6b42f2aa0fdcfc1176a87b5
Eliran Ben Maor <eliranbenmaor@gmail.com>
2023-10-29 17:49:15 +0200

    added modules directory

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
 }
 
 module "compute" {
-  source = "git::https://github.com/snrow/Terraform-class-ex5//compute"
+  source = "git::https://github.com/snrow/Terraform-class-ex5/modules//compute"
 
     instance_my_subnet_id = "insert your subnet id>"
 }
 
 module "network" {
-  source = "git::https://github.com/snrow/Terraform-class-ex5//network"
+  source = "git::https://github.com/snrow/Terraform-class-ex5/modules//network"
 
   target_vpc_id = "<insert your vpc id>"
 }
